466e69785db73225cdfb51b6367e86c1470a5807
Huy Vu <tumlum129@gmail.com>
2015-12-25 21:53:30 -0600

    Initial Commit

---
+++ gittest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest2
+//
+//  Created by Huy Vu on 12/25/15.
+//  Copyright © 2015 Huy Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
